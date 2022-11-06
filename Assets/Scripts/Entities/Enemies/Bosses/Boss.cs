d538d1405784d4e27d43a93ddc5a10cc6a3727fb
araysar <misu.kaze@gmail.com>
2022-11-06 05:20:11 -0300

    n

---
+++ Assets/Scripts/Entities/Enemies/Bosses/Boss.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Boss : MonoBehaviour
+{
+    protected Animator myAnim;
+    protected Rigidbody2D myRb;
+    protected Health myHealth;
+    protected BossFightManager myManager;
+    
+    void Awake()
+    {
+        myAnim = GetComponent<Animator>();
+        myRb = GetComponent<Rigidbody2D>();
+        myHealth = GetComponent<Health>();
+        myManager = FindObjectOfType<BossFightManager>();
+    }
+
+    public abstract void StartingFight();
+    public abstract void GenerateShield();
+    public abstract void SelectAttack();
+    public abstract void FinishAttack();
+    public abstract void Respawn();
+
+    public abstract void StopMovement();
+    public abstract void ResumeMovement();
+}
--- Assets/Scripts/Managers/BossFightManager.cs
+++ Assets/Scripts/Managers/BossFightManager.cs
@@ -1,6 +1,6 @@
+using Cinemachine;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossFightManager : MonoBehaviour
@@ -8,14 +8,45 @@ public class BossFightManager : MonoBehaviour
     [SerializeField] private TriggerBossDoor myTrigger;
     [SerializeField] private GameObject myCamera;
 
-    private event Action ResetFightAction;
+    [SerializeField] private CinemachineBlendDefinition.Style myTransitionEffect;
+    [SerializeField] private float myTransitionDuration;
+    private CinemachineBrain myCameraBrain;
+
+    public event Action EnteringBossDoorEvent;
+    public event Action StartFightEvent;
+    public event Action ResetFightActionEvent;
+
     void Start()
     {
-        ResetFightAction += myTrigger.ResetPosition;
+        myCameraBrain = FindObjectOfType<CinemachineBrain>();
+        ResetFightActionEvent += myTrigger.ResetPosition;
+        EnteringBossDoorEvent += Character_Movement.instance.StopMovement;
+        StartFightEvent += Character_Movement.instance.ResumeMovement;
     }
 
+    public void EnteringBossDoor()
+    {
+        StartCoroutine(EnteringTimer());
+    }
+
+    private IEnumerator EnteringTimer()
+    {
+        EnteringBossDoorEvent();
+        yield return new WaitForSeconds(1);
+        myCameraBrain.m_DefaultBlend.m_Style = myTransitionEffect;
+        myCameraBrain.m_DefaultBlend.m_Time = myTransitionDuration;
+        myCamera.SetActive(true);
+        yield return new WaitForSeconds(myTransitionDuration);
+
+    }
     public void TriggerBattle()
     {
-        
+        StartFightEvent();
+    }
+
+    public void ResetBattle()
+    {
+        myCameraBrain.m_DefaultBlend.m_Style = CinemachineBlendDefinition.Style.Cut;
+        myCamera.SetActive(false);
     }
 }
