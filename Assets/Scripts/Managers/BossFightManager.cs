72236cd644084fa2373381306156edfd139237d9
araysar <misu.kaze@gmail.com>
2022-11-06 05:37:10 -0300

    Revert "n"
    This reverts commit d538d1405784d4e27d43a93ddc5a10cc6a3727fb.

--- Assets/Scripts/Managers/BossFightManager.cs
+++ Assets/Scripts/Managers/BossFightManager.cs
@@ -1,6 +1,6 @@
-using Cinemachine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BossFightManager : MonoBehaviour
@@ -8,45 +8,14 @@ public class BossFightManager : MonoBehaviour
     [SerializeField] private TriggerBossDoor myTrigger;
     [SerializeField] private GameObject myCamera;
 
-    [SerializeField] private CinemachineBlendDefinition.Style myTransitionEffect;
-    [SerializeField] private float myTransitionDuration;
-    private CinemachineBrain myCameraBrain;
-
-    public event Action EnteringBossDoorEvent;
-    public event Action StartFightEvent;
-    public event Action ResetFightActionEvent;
-
+    private event Action ResetFightAction;
     void Start()
     {
-        myCameraBrain = FindObjectOfType<CinemachineBrain>();
-        ResetFightActionEvent += myTrigger.ResetPosition;
-        EnteringBossDoorEvent += Character_Movement.instance.StopMovement;
-        StartFightEvent += Character_Movement.instance.ResumeMovement;
+        ResetFightAction += myTrigger.ResetPosition;
     }
 
-    public void EnteringBossDoor()
-    {
-        StartCoroutine(EnteringTimer());
-    }
-
-    private IEnumerator EnteringTimer()
-    {
-        EnteringBossDoorEvent();
-        yield return new WaitForSeconds(1);
-        myCameraBrain.m_DefaultBlend.m_Style = myTransitionEffect;
-        myCameraBrain.m_DefaultBlend.m_Time = myTransitionDuration;
-        myCamera.SetActive(true);
-        yield return new WaitForSeconds(myTransitionDuration);
-
-    }
     public void TriggerBattle()
     {
-        StartFightEvent();
-    }
-
-    public void ResetBattle()
-    {
-        myCameraBrain.m_DefaultBlend.m_Style = CinemachineBlendDefinition.Style.Cut;
-        myCamera.SetActive(false);
+        
     }
 }
