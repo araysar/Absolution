e3a73260dcc9d6f1b47c50655618fc803481d3a2
araysar <87583237+araysar@users.noreply.github.com>
2023-04-17 16:26:19 -0300

    as

--- Assets/Scripts/Entities/Player/Character_Attack.cs
+++ Assets/Scripts/Entities/Player/Character_Attack.cs
@@ -55,7 +55,7 @@ public class Character_Attack : MonoBehaviour
         CreateCube();
         player = GetComponent<Character_Movement>();
         currentTime = timeToShuffle;
-        currentAttack = myAttacks[1];// myAttacks[Random.Range(0, myAttacks.Length)];
+        currentAttack = myAttacks[0];// myAttacks[Random.Range(0, myAttacks.Length)];
         uiImage.sprite = currentAttack.myImage;
     }
 
