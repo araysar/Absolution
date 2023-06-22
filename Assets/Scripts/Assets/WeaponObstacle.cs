1ffdd014522ac6b95ffdeb6e8d8fad487cbea2dc
araysar <87583237+araysar@users.noreply.github.com>
2023-06-22 03:50:29 -0300

    asd

--- Assets/Scripts/Assets/WeaponObstacle.cs
+++ Assets/Scripts/Assets/WeaponObstacle.cs
@@ -6,19 +6,12 @@ public class WeaponObstacle : MonoBehaviour
     [SerializeField] private GameObject destroyEffect;
     [SerializeField] private AudioClip destroySfx;
     [SerializeField] private DamageStay myDamage;
-    private void Start()
-    {
-        GameManager.instance.EnemyRespawnEvent += Enable;
-    }
 
     private void Enable()
     {
         gameObject.SetActive(true);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
 
-    }
     public void Destroy()
     {
         if (destroyEffect != null) Instantiate(destroyEffect, transform.position, Quaternion.identity);
