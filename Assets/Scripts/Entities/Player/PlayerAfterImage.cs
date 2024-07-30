0dd2d077c48d8dc16917f077d424611735858f90
araysar <87583237+araysar@users.noreply.github.com>
2024-07-30 10:55:47 -0300

    asd
    asd

--- Assets/Scripts/Entities/Player/PlayerAfterImage.cs
+++ Assets/Scripts/Entities/Player/PlayerAfterImage.cs
@@ -5,41 +5,32 @@ using UnityEngine;
 public class PlayerAfterImage : MonoBehaviour
 {
     [SerializeField] private float activeTime = 0.1f;
-    private float timeActivated;
-    private float alpha;
-    [SerializeField] private float alphaSet = 0.8f;
-    private float alphaMultiplier = 0.85f;
+
 
     [HideInInspector] public PlayerAfterImagePool imagePool;
     private Transform player;
     private SpriteRenderer sR;
-    private SpriteRenderer playerSR;
-
-    public Color color;
 
     private void OnEnable()
     {
         sR = GetComponent<SpriteRenderer>();
         if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerSR = player.GetComponent<SpriteRenderer>();
 
-        alpha = alphaSet;
-        sR.sprite = playerSR.sprite;
+        sR.sprite = player.GetComponent<SpriteRenderer>().sprite;
         transform.position = player.transform.position;
         transform.rotation = player.transform.rotation;
         transform.localScale = player.transform.localScale;
-        timeActivated = Time.time;
+        StartCoroutine(Timer());
     }
 
-    private void Update()
+    IEnumerator Timer()
     {
-        alpha = alphaMultiplier;
-        color.a = alpha;
-        sR.color = color;
-
-        if(Time.time >= (timeActivated + activeTime))
+        sR.material.SetFloat("_DeathValue", 0);
+        for (float i = 0; i < activeTime; i += Time.deltaTime)
         {
-            imagePool.AddToPool(gameObject);
+            sR.material.SetFloat("_DeathValue", i  / activeTime);
+            yield return null;
         }
+        imagePool.AddToPool(gameObject);
     }
 }
